a72c4a2ff1f0d113fb2fd15a711c389c6b2c4a64
xtine burrough <xtineburrough@gmail.com>
2025-05-16 15:48:18 +0000

    finished 6-2

--- assets/js/main-6-2.js
+++ assets/js/main-6-2.js
@@ -1,4 +1,13 @@
 // main.js
 
 console.log("Hello, World!");
-document.body.style.opacity = .5;
+document.body.style.transform = `rotate(90deg)`;
+
+// Rotate the body 90 degrees
+let rotation = 0;
+document.addEventListener("click", function () {
+let min = 10;
+let max = 40;
+rotation += Math.random() * (max - min) + min;
+document.body.style.transform = `rotate(${rotation}deg)`;
+});
